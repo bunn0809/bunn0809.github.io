db13b06b8a0fa3347e72c8006c86a31a039e4e9c
bunn0809 <98290237+bunn0809@users.noreply.github.com>
2022-03-08 09:55:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,28 @@
   let defense = document.getElementById("defense_btn");   
   let bossHp = 100
   let myHp = 100
-  attack.addEventListener("click", () => {
+  
+  const myHptext =  document.createElement("p");
+  // document.getElementsByTagName("body")[0].appendChild(myHptext);
+    document.getElementById("yushap").append(myHptext);
+    
+    
+    
+    attack.addEventListener("click", () => {
       const damage1 = Math.floor(Math.random() * 20) + 1;
       const damage2 = Math.floor(Math.random() * 20) + 1;
-      bossHp -= damage1
-      myHp -= damage2
-      console.log(bossHp, myHp)
+      bossHp -= damage1;
+      myHp -= damage2;
+      myHptext.textContent = myHp;
+      console.log(bossHp, myHp);
   })
   defense.addEventListener("click", () => {
       const damage = Math.floor(Math.random() * 20) + 1;
-      myHp -= damage / 2
-      console.log(bossHp, myHp)
+      myHp -= damage / 2;
+      console.log(bossHp, myHp);
   })
   reset.addEventListener("click", () => {
     myHp = 0 && window.confirm("HP前回復")
-    console.log(bossHp, myHp)
+    console.log(bossHp, myHp);
 })
   
